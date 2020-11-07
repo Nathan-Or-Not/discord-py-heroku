36469a61fc161ea5154a408ea4f39521a4ad6fd1
Nathan-Or-Not <nmarbles22@gmail.com>
2020-11-06 18:20:07 -0800

    testing roll command

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="n!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
@@ -11,6 +11,10 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+@bot.command()
+async def roll(ctx):
+    await ctx.send(f"{ctx.message.author.mention} rolled {random.randrange(1,101)}!")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
